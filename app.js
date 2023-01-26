3dda818331283db6d5747710d47595dd54e6a70a
abir364 <abirh364@gmail.com>
2023-01-27 05:13:22 +0600

    clear and backspace button running fixed some NaN related bugs

--- app.js
+++ app.js
@@ -9,10 +9,21 @@ const numbers = document.querySelectorAll('#number');
 
 numbers.forEach((number) => {
     number.addEventListener('click', ()=>{
+        //2 line display
+        //top is top line of display
         const top = document.querySelector('.top-line');
+        //bottom is bottom line of display
         const bottom = document.querySelector('.bottom-line');
+        //operator displays current operator on left side of display
         const operation = document.querySelector('.operation');
-        if ((number.innerHTML === '+' || number.innerHTML === '-' || 
+        //x(+,-,*,/)
+        if ((number.innerHTML ==='×' || number.innerHTML === '÷')
+        && firstOperand === '' && s ==='') {
+            //alert a reset to initial state and run ac function
+            alert("Reset to initial state")
+            ac();
+        }
+        else if ((number.innerHTML === '+' || number.innerHTML === '-' || 
             number.innerHTML ==='×' || number.innerHTML === '÷') 
             && firstOperand ==='' && !hasAnsStored) {
             firstOperand = s;
@@ -22,6 +33,7 @@ numbers.forEach((number) => {
             operator = number.innerHTML;
             operation.innerHTML = operator;
         }
+        //ans(+,-,*,/)
         else if ((number.innerHTML === '+' || number.innerHTML === '-' || 
         number.innerHTML ==='×' || number.innerHTML === '÷') 
         && hasAnsStored) {
@@ -32,6 +44,12 @@ numbers.forEach((number) => {
             bottom.innerHTML = '';
             s = '';
         }
+        else if ((number.innerHTML === '+' ||  number.innerHTML ==='×' || 
+        number.innerHTML === '÷') && operator !== '') {
+            alert("multiple operators can not be used simultaneously");
+            ac();
+        } 
+        //x(+,-,*,/)y(+,-,*,/)
         else if ((number.innerHTML === '+' || number.innerHTML === '-' || 
         number.innerHTML ==='×' || number.innerHTML === '÷') 
         && firstOperand!=='' && s!=='') {
@@ -43,7 +61,11 @@ numbers.forEach((number) => {
             bottom.innerHTML = '';
             s = '';
         }
-        else if (number.innerHTML === '=' && firstOperand !== '' && operator !== '') {
+        else if (number.innerHTML === '=' && s === '0') {
+            alert("Why U try to make me do these pointless things?");
+        }
+        //x(+,-,*,/)y=
+        else if (number.innerHTML === '=' && firstOperand !== '' && operator !== '' && s !== '') {
             secondOperand = s;
             answer = doMath(firstOperand,secondOperand,operator).toString();
             hasAnsStored = true;
@@ -55,39 +77,48 @@ numbers.forEach((number) => {
             s = '';
             operator = '';
         }
+        else if (number.innerHTML === 'clear') {
+            ac();
+        }
+        else if (number.innerHTML === 'backspace' && !hasAnsStored) {
+            s = s.slice(0, -1);
+            bottom.innerHTML = s;
+        }
+        else if (number.innerHTML === 'backspace' && hasAnsStored) {
+            answer = answer.slice(0, -1);
+            top.innerHTML = answer;
+            s = top.innerHTML;
+        }
         else if (!hasAnsStored && number.innerHTML !== '=') {
             s += number.innerHTML;
             bottom.innerHTML = s;
         }
-        else if(hasAnsStored && operator ==='') {
+        else if(hasAnsStored && operator ==='' && number.innerHTML !== '=') {
             hasAnsStored = false;
             answer = '';
             top.innerHTML = '';
             s += number.innerHTML;
             bottom.innerHTML = s;
         }
-        else if(hasAnsStored && operator !=='') {
-            console.log(bottom.innerHTML);
+        else if(hasAnsStored && operator !=='' && number.innerHTML !== '=') {
             hasAnsStored = false;
             answer = '';
             s += number.innerHTML;
             bottom.innerHTML = s;
         }
+        //= error alert
         else if (number.innerHTML === '=' && firstOperand === '' &&  operator === '') {
             alert("Enter number before proceeding");
         }
-        /*else if (number.innerHTML === '=' && firstOperand !== '' 
-        && operator !== '' && s ==='' && secondOperand === '') {
-            alert("Enter number before proceeding"); 
-            //this condition is checks bugs i.e '4+='
+        //x(+,-,*,/)= error alert
+        else if (number.innerHTML === '=' && firstOperand !== '' 
+        && operator !== '' && secondOperand === '') {
+            alert("Enter number before proceeding");
         }
-        else if (number.innerHTML === '=' && top.innerHTML === NaN) {
-            //block operations on NaN values
-        }*/
     });
 });
 
-function doMath(a,b,operator){
+function doMath(a,b,operator) {
     let opA, opB;
     if (a.includes('.')){
         opA = parseFloat(a);
@@ -111,4 +142,22 @@ function doMath(a,b,operator){
         return (opA / opB);
     }
     
+}
+
+function ac() {
+    s='';
+    firstOperand='';
+    secondOperand='';
+    operator='';
+    hasAnsStored = false;
+    answer = '';
+    //top is top line of display
+    const top = document.querySelector('.top-line');
+    top.innerHTML = '';
+    //bottom is bottom line of display
+    const bottom = document.querySelector('.bottom-line');
+    bottom.innerHTML = '';
+    //operator displays current operator on left side of display
+    const operation = document.querySelector('.operation');
+    operation.innerHTML = '';
 }
